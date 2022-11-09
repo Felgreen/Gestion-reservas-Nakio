27b98d66dd6c52e4f7787db268b49777911ea339
jfelici <jfelici@agea.com.ar>
2022-11-09 20:36:04 -0300

    services filled up and links to calendly events added

--- src/components/ui/Services.jsx
+++ src/components/ui/Services.jsx
@@ -12,7 +12,7 @@ const Services = ({option}) => {
                         <h1 className="text-2xl sm:text-6xl font-bold text-[#F36E21]">Basic service</h1>
                         <hr className="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col max-w-[700px]">
                         <p className="text-2xl sm:text-2xl font-bold text-white max-w-[700px]">
                         This service includes all the primary care your vehicle needs to keep you on the move and avoid expensive unwanted surprises. 
                         </p>
@@ -24,11 +24,12 @@ const Services = ({option}) => {
                             <li>- Visual Inspection of breaks, steering components, suspension and liquid levels.</li>
                         </ul>
                     </div>
+                    <br></br>
                     <div>
-                        <a href="https://calendly.com/nacho-coppa/basic-service" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
+                        <a href="https://calendly.com/nacho-coppa/basic-service" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1.5rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
                     </div>
                 </div>
-            )
+            );
         case 'extend':
             return (
                 <div className="flex flex-col justify-center w-full p-10 items-center space-y-3">
@@ -37,7 +38,7 @@ const Services = ({option}) => {
                         <h1 className="text-2xl sm:text-6xl font-bold text-[#F36E21]">Extend my car life service:</h1>
                         <hr className="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col max-w-[700px]">
                         <p className="text-2xl sm:text-2xl font-bold text-white max-w-[700px]">
                             Want to maximize the life of your trustworthy motor vehicle? We can help. This service includes all of the Basic Service package  and adds on the following maintenance services: 
                         </p>
@@ -51,28 +52,23 @@ const Services = ({option}) => {
                             <li>- Belt check</li>
                         </ul>
                     </div>
+                    <br></br>
                     <div>
-                        <a href="https://calendly.com/nacho-coppa/extend-my-life-car" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
+                        <a href="https://calendly.com/nacho-coppa/extend-my-life-car" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1.5rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
                     </div>
                 </div>
             );
         case 'presell':
             return (
                 <div className="flex flex-col justify-center w-full p-10 items-center space-y-3">
-                    <div className="inline-flex justify-center items-center w-full">
+                    <div className="inline-flex justify-center align-center items-center w-full">
                         <hr className="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
                         <h1 className="text-2xl sm:text-6xl font-bold text-[#F36E21]">Pre sell service</h1>
                         <hr className="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
                     </div>
-                    <div className="flex flex-col">
-                        <p className="text-2xl sm:text-2xl font-bold text-white max-w-[700px]">
+                    <div className="flex flex-col max-w-[700px]">
+                        <p className="text-2xl sm:text-2xl font-bold">
                             Avoid getting scammed and get a trustworthy opinion on the suspension, steering, leaks, tires, transmission and basic state of a vehicle. 
-                        </p>
-                        <p>
-                            Basic Walk Around + Deep Inspection
-                        </p>
-                        <p>
-                            To continue reducing the risk of buying an overpriced , problem generating day ruining motor vehicle use this service to check the oil, gearbox and reduction gear.
                         </p>
                         <br></br>
                         <ul>
@@ -80,9 +76,18 @@ const Services = ({option}) => {
                             <li>- Dent removal</li>
                             <li>- List the sale to my network of contacts.</li>
                         </ul>
+                        <br></br>
+                        <p className="text-2xl sm:text-2xl font-bold text-white max-w-[700px]">
+                            Deep inspection available!
+                        </p>
+                        <br></br>
+                        <p>
+                            To continue reducing the risk of buying an overpriced , problem generating day ruining motor vehicle use this service to check the oil, gearbox and reduction gear.
+                        </p>
                     </div>
+                    <br></br>
                     <div>
-                        <a href="https://calendly.com/nacho-coppa/pre-sell-service" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
+                        <a href="https://calendly.com/nacho-coppa/pre-sell-service" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1.5rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
                     </div>
                 </div>
             );
@@ -94,7 +99,7 @@ const Services = ({option}) => {
                         <h1 className="text-2xl sm:text-6xl font-bold text-[#F36E21]">4wd upgrade</h1>
                         <hr className="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col max-w-[700px]">
                         <p className="text-2xl sm:text-2xl font-bold text-white max-w-[700px]">
                             Lets get that car ready for any terrain. Ask for a quote on component upgrade and installation. 
                         </p>
@@ -109,8 +114,9 @@ const Services = ({option}) => {
                             <li>- Off road tires and more!</li>
                         </ul>
                     </div>
+                    <br></br>
                     <div>
-                        <a href="https://calendly.com/nacho-coppa/4wd-upgrade" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
+                        <a href="https://calendly.com/nacho-coppa/4wd-upgrade" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1.5rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
                     </div>
                 </div>
             );
@@ -127,8 +133,9 @@ const Services = ({option}) => {
                             “Is it supposed to make this noise?” If you don't know what´s going on with you car and want to get it inspected? Reserve an inspection!"
                         </p>
                     </div>
+                    <br></br>
                     <div>
-                        <a href="https://calendly.com/nacho-coppa/whats-that-noise" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
+                        <a href="https://calendly.com/nacho-coppa/whats-that-noise" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1.5rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
                     </div>
                 </div>
             );
@@ -139,14 +146,21 @@ const Services = ({option}) => {
                     <h1 className="text-2xl sm:text-6xl font-bold text-[#F36E21]">Basic service</h1>
                     <hr className="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
                 </div>
-                <div className="flex">
+                <div className="flex flex-col max-w-[700px]">
                     <p className="text-2xl sm:text-2xl font-bold text-white max-w-[700px]">
-                        Replace of: oil, oil filter, fuel filter, air filter <br></br>
-                        Visual inspection of: breaks, steering components, suspension, liquid levels
+                    This service includes all the primary care your vehicle needs to keep you on the move and avoid expensive unwanted surprises. 
                     </p>
+                    <br></br>
+                    <ul>
+                        <li>- Oil / oil filter change</li>
+                        <li>- Replace Air Filter</li>
+                        <li>- Replace Fuel Filter</li>
+                        <li>- Visual Inspection of breaks, steering components, suspension and liquid levels.</li>
+                    </ul>
                 </div>
+                <br></br>
                 <div>
-                    <a href="https://calendly.com/nacho-coppa/basic-service" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
+                    <a href="https://calendly.com/nacho-coppa/basic-service" className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[1.5rem] font-semibold leading-5 text-white hover:bg-indigo-500">Reservar turno</a>
                 </div>
             </div>
         );
